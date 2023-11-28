3c6f1b1350626eeeadaf82b4d7a64f430ae10e9d
nakabonne <ryo@nakao.dev>
2023-11-28 20:12:10 +0900

    Add autoscaling group resource

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,11 +13,86 @@ provider "aws" {
   region = var.aws_region
 }
 
-resource "aws_instance" "instance" {
-  ami           = var.ami_id
-  instance_type = var.instance_type
+resource "aws_autoscaling_group" "autoscaling_group" {
+  name_prefix = var.cluster_name
 
-  tags = {
-    Name = "weave-server"
+  launch_configuration = aws_launch_configuration.launch_configuration.name
+
+  # Run a fixed number of instances in the ASG
+  min_size         = var.cluster_size
+  max_size         = var.cluster_size
+  desired_capacity = var.cluster_size
+
+  tag {
+    key                 = "Name"
+    value               = "weave"
+    propagate_at_launch = true
   }
+}
+
+resource "aws_launch_configuration" "launch_configuration" {
+  name_prefix   = "${var.cluster_name}-"
+  image_id      = var.ami_id
+  instance_type = var.instance_type
+  user_data     = var.user_data
+
+
+  security_groups = concat(
+    [aws_security_group.lc_security_group.id],
+    [],
+  )
+}
+
+resource "aws_security_group" "lc_security_group" {
+  name_prefix = var.cluster_name
+  description = "Security group for the ${var.cluster_name} launch configuration"
+  #vpc_id      = var.vpc_id
+
+  # aws_launch_configuration.launch_configuration in this module sets create_before_destroy to true, which means
+  # everything it depends on, including this resource, must set it as well, or you'll get cyclic dependency errors
+  # when you try to do a terraform destroy.
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  tags = merge(
+    {
+      "Name" = var.cluster_name
+    },
+    [],
+  )
+}
+
+# TODO: Add security group rules
+
+resource "aws_security_group_rule" "allow_ssh_inbound" {
+  count       = length(var.allowed_ssh_cidr_blocks) >= 1 ? 1 : 0
+  type        = "ingress"
+  from_port   = var.ssh_port
+  to_port     = var.ssh_port
+  protocol    = "tcp"
+  cidr_blocks = var.allowed_ssh_cidr_blocks
+
+  security_group_id = aws_security_group.lc_security_group.id
+}
+
+resource "aws_security_group_rule" "allow_ssh_inbound_from_security_group_ids" {
+  count                    = var.allowed_ssh_security_group_count
+  type                     = "ingress"
+  from_port                = var.ssh_port
+  to_port                  = var.ssh_port
+  protocol                 = "tcp"
+  source_security_group_id = element(var.allowed_ssh_security_group_ids, count.index)
+
+  security_group_id = aws_security_group.lc_security_group.id
+}
+
+resource "aws_security_group_rule" "allow_all_outbound" {
+  type        = "egress"
+  from_port   = 0
+  to_port     = 0
+  protocol    = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
+
+  security_group_id = aws_security_group.lc_security_group.id
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,14 +1,33 @@
 variable "aws_region" {
   type        = string
   description = "aws region"
+  default     = "ap-northeast-1"
+}
+
+variable "cluster_name" {
+  type        = string
+  description = "cluster name"
+  default     = "weave"
 }
 
 variable "instance_type" {
   type        = string
   description = "instance type"
+  default     = "t2.small"
 }
 
 variable "ami_id" {
   type        = string
   description = "AMI ID"
+}
+
+variable "cluster_size" {
+  description = "The number of nodes to have in the Consul cluster. We strongly recommended that you use either 3 or 5."
+  type        = number
+  default     = 2
+}
+
+variable "user_data" {
+  description = "A User Data script to execute while the server is booting."
+  type        = string
 }
