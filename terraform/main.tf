a6a1ac74e9398ae222e9abdc7f6d4e3efdc1af13
nakabonne <ryo@nakao.dev>
2023-12-06 10:48:46 +0900

    Use inline security group

--- terraform/main.tf
+++ terraform/main.tf
@@ -60,7 +60,57 @@ data "template_file" "weave_bootstrap" {
 resource "aws_security_group" "lc_security_group" {
   name_prefix = var.cluster_name
   description = "Security group for the ${var.cluster_name} launch configuration"
-  #vpc_id      = var.vpc_id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Weave TCP for itself"
+    from_port   = 6783
+    to_port     = 6783
+    protocol    = "tcp"
+    self        = true
+  }
+
+  ingress {
+    description = "Weave UDP for itself"
+    from_port   = 6783
+    to_port     = 6784
+    protocol    = "udp"
+    self        = true
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8 # Echo Request
+    to_port     = 0
+    protocol    = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   # aws_launch_configuration.launch_configuration in this module sets create_before_destroy to true, which means
   # everything it depends on, including this resource, must set it as well, or you'll get cyclic dependency errors
@@ -68,78 +118,6 @@ resource "aws_security_group" "lc_security_group" {
   lifecycle {
     create_before_destroy = true
   }
-}
-
-resource "aws_security_group_rule" "allow_all_outbound" {
-  type        = "egress"
-  from_port   = 0
-  to_port     = 0
-  protocol    = "-1"
-  cidr_blocks = ["0.0.0.0/0"]
-
-  security_group_id = aws_security_group.lc_security_group.id
-}
-
-resource "aws_security_group_rule" "allow_ssh_inbound" {
-  type        = "ingress"
-  from_port   = 22
-  to_port     = 22
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-
-  security_group_id = aws_security_group.lc_security_group.id
-}
-
-
-resource "aws_security_group_rule" "allow_tcp_inbound" {
-  type        = "ingress"
-  from_port   = 6783
-  to_port     = 6783
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-
-  security_group_id = aws_security_group.lc_security_group.id
-}
-
-resource "aws_security_group_rule" "allow_http_inbound" {
-  type        = "ingress"
-  from_port   = 80
-  to_port     = 80
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-
-  security_group_id = aws_security_group.lc_security_group.id
-}
-
-resource "aws_security_group_rule" "allow_https_inbound" {
-  type        = "ingress"
-  from_port   = 443
-  to_port     = 443
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-
-  security_group_id = aws_security_group.lc_security_group.id
-}
-
-resource "aws_security_group_rule" "allow_udp_inbound_6783" {
-  type        = "ingress"
-  from_port   = 6783
-  to_port     = 6784
-  protocol    = "udp"
-  cidr_blocks = ["0.0.0.0/0"]
-
-  security_group_id = aws_security_group.lc_security_group.id
-}
-
-
-resource "aws_security_group_rule" "allow_icmp_inbound" {
-  type        = "ingress"
-  from_port   = 8 # Echo Request
-  to_port     = 0
-  protocol    = "icmp"
-  cidr_blocks = ["0.0.0.0/0"]
-
-  security_group_id = aws_security_group.lc_security_group.id
 }
 
 data "aws_iam_policy_document" "assume_role_policy" {
